bd0da534027a93e7f303af6c56d7562dae22fb7d
Nupur Singh <nupur.singh2410@gmail.com>
2024-04-11 13:45:38 +0530

    4th stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,46 +9,55 @@ public class Main {
     static String CLRF= "\r\n";
     static String EOSL= "\r\n";
     public static void main(String[] args) {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
 
-        // Uncomment this block to pass the first stage
 
-        /**
-         * ServerSocket object binds itself to a particular port on server
-         * machine. The port acts like an address for incoming client requests
-         * The server continously listens to this port, waiting for clients to
-         * initate communication. When a client tries to connect to the servers port,
-         * the ServerSocket accepts the communication request. This creates a new Socket object
-         * representing the established connectin b/w client and server.
-         */
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
         try {
             serverSocket = new ServerSocket(4221);
             serverSocket.setReuseAddress(true);
-            //This method blocks the server's execution until a client connects. Once a client
-            //is established, accept() returns a new Socket() object
-            clientSocket = serverSocket.accept(); // Wait for connection from client.
-            BufferedReader br= new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            DataOutputStream dataOut= new DataOutputStream(clientSocket.getOutputStream());
+            clientSocket = serverSocket.accept();
+            handleRequest(clientSocket);
+        } catch (IOException e) {
+            System.out.println("IOExcep tion: " + e.getMessage());
+        }
+    }
+
+    private static void handleRequest(Socket clientSocket) throws IOException {
+        try(BufferedReader br= new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            DataOutputStream dataOut= new DataOutputStream(clientSocket.getOutputStream());)
+        {
             StringBuilder requestBuilder = new StringBuilder();
             String line;
             while ((line = br.readLine()) != null && !line.isEmpty()) {
                 requestBuilder.append(line).append("\r\n");
             }
             String request = requestBuilder.toString();
-            String[] requestInParts= request.split(" ");
-            String path= null;
-            if(requestInParts.length > 2) {
-                path= requestInParts[1];
+            int contentLength= request.getBytes().length;
+            System.out.println("content length is " +contentLength);
+            String[] requestInParts = request.split(" ");
+            String path = null;
+            if (requestInParts.length > 1) {
+                path = requestInParts[1];
             }
-            if("/".equals(path)) dataOut.writeBytes(OK_200+CLRF+EOSL);
-            else dataOut.writeBytes(NOT_FOUND_404+CLRF+EOSL);
+            if ("/".equals(path)) {
+                dataOut.writeBytes(OK_200 + CLRF + EOSL);
+            } else if (path != null && path.contains("/echo")) {
+                path = path.split("/")[2];
+                dataOut.writeBytes(OK_200 + CLRF
+                        + "Content-Type: text/plain" + CLRF
+                        + "Content-Length: " + contentLength + CLRF
+                        + CLRF
+                        + path);
+            } else {
+                dataOut.writeBytes(NOT_FOUND_404 + CLRF + EOSL);
+            }
             dataOut.flush();
             System.out.println("accepted new connection");
-        } catch (IOException e) {
-            System.out.println("IOExcep tion: " + e.getMessage());
+        } finally {
+            clientSocket.close();
         }
-    }
+        }
+
 }
